d9571f1c2a5e6fd9825f2297ff4ecceae7d0375e
Goshik <georgesorokin0905@gmail.com>
2022-04-06 09:20:11 +0300

    third repo

---
+++ cmake-build-debug/CMakeFiles/4_1sem.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "4_1sem.exe"
+  "4_1sem.exe.manifest"
+  "4_1sem.pdb"
+  "CMakeFiles/4_1sem.dir/CVector.cpp.obj"
+  "CMakeFiles/4_1sem.dir/main.cpp.obj"
+  "lib4_1sem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4_1sem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
